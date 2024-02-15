f0f8fd753f8f6bd76bf89f359a5a4fcfc0cc2cc5
Mikael Barros <mikael.barros@tcema.tc.br>
2024-02-15 10:08:07 -0300

    feat: finalizes relationship between order and product

--- src/main/java/com/example/commerce/entities/Product.java
+++ src/main/java/com/example/commerce/entities/Product.java
@@ -4,6 +4,7 @@ package com.example.commerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -85,4 +86,11 @@ public class Product {
         return categories;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
+    }
 }
